8e446c9c7abfbdb5bafb6062cedb67b16dda0cc9
Ninux <131157070+N1nux@users.noreply.github.com>
2024-05-24 23:18:23 +0200

    Flute Sounds
    Started adding sounds to the flute

--- Assets/Scripts/Interactable/FluteScript.cs
+++ Assets/Scripts/Interactable/FluteScript.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class FluteScript : MonoBehaviour
 {
@@ -14,21 +13,87 @@ public class FluteScript : MonoBehaviour
     public RawImage note4;
     public RawImage note5;
     public RawImage note6;
+    public RawImage note7;
     public KeyCode note1Key;
     public KeyCode note2Key;
     public KeyCode note3Key;
     public KeyCode note4Key;
     public KeyCode note5Key;
     public KeyCode note6Key;
+    public KeyCode note7Key;
+    public AudioClip note01Audio;
+    public AudioClip note02Audio;
+    public AudioClip note03Audio;
+    public AudioClip note04Audio;
+    public AudioClip note05Audio;
+    public AudioClip note06Audio;
+    public AudioClip note07Audio;
+    //public AudioClip note08Audio;
+    //public AudioClip note09Audio;
+    //public AudioClip note10Audio;
+    //public AudioClip note11Audio;
+    //public AudioClip note12Audio;
+    //public AudioClip note13Audio;
+
+    private AudioSource audioSource;
+    private Dictionary<string, AudioClip> noteMapping;
 
     // Start is called before the first frame update
     void Start()
     {
 
+        audioSource = GetComponent<AudioSource>();
+
+        // Initialize the mapping of button combinations to notes
+        noteMapping = new Dictionary<string, AudioClip>
+        {
+            { "1111111", note01Audio }, // D
+            { "1111101", note02Audio }, // E
+            { "1111001", note03Audio }, // F
+            { "1110001", note04Audio }, // G
+            { "1100001", note05Audio }, // A
+            { "1000001", note06Audio }, // H
+            { "0100001", note07Audio }, // C"
+            //{ "0100000", note08Audio }, // D"
+            //{ "1111100", note09Audio }, // E"
+            //{ "1111000", note10Audio }, // F"
+            //{ "1110000", note11Audio }, // G"
+           // { "1100000", note12Audio }, // A"
+           // { "1101100", note13Audio }, // C"
+        };
     }
 
     // Update is called once per frame
     void Update()
+    {
+        displayUI();
+
+        // Check which keys are pressed and form a key string
+        string key = (Input.GetKey(note1Key) ? "1" : "0") +
+                     (Input.GetKey(note2Key) ? "1" : "0") +
+                     (Input.GetKey(note3Key) ? "1" : "0") +
+                     (Input.GetKey(note4Key) ? "1" : "0") +
+                     (Input.GetKey(note5Key) ? "1" : "0") +
+                     (Input.GetKey(note6Key) ? "1" : "0") +
+                     (Input.GetKey(note7Key) ? "1" : "0");
+
+        // Play the corresponding note if the combination exists in the dictionary
+        if (noteMapping.ContainsKey(key))
+        {
+            PlaySound(noteMapping[key]);
+        }
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (audioSource.clip != clip || !audioSource.isPlaying)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+
+    private void displayUI()
     {
         // If the Flute is in the inventory, display the flute UI
         if (itemPickup.getInventory() == fluteObject)
@@ -44,8 +109,9 @@ public class FluteScript : MonoBehaviour
             note4.enabled = false;
             note5.enabled = false;
             note6.enabled = false;
+            note7.enabled = false;
         }
-        
+
         if (Input.GetKeyDown(note1Key))
         {
             note1.enabled = true;
@@ -98,6 +164,15 @@ public class FluteScript : MonoBehaviour
         else if (Input.GetKeyUp(note6Key))
         {
             note6.enabled = false;
+        }
+
+        if (Input.GetKeyDown(note7Key))
+        {
+            note7.enabled = true;
+        }
+        else if (Input.GetKeyUp(note7Key))
+        {
+            note7.enabled = false;
         }
     }
 }
