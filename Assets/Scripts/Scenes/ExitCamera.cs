4f2cc90511d7c78031f8b6b01d54c45522c91a18
Henri Stimm <73344515+capemsx@users.noreply.github.com>
2024-05-29 21:30:20 +0200

    added cutscene, titlescreen

---
+++ Assets/Scripts/Scenes/ExitCamera.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+
+public class ExitCamera : MonoBehaviour
+{
+    public SceneLoader sceneLoader;
+    public string sceneToLoad;
+    private void Start()
+    {
+        StartCoroutine(SwitchCamera());
+    }
+
+    private IEnumerator SwitchCamera()
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(10f);
+        sceneLoader.LoadNextScene(sceneToLoad);
+            Destroy(this);
+    }
+}
